4219a96a2c515ba4a485590cf2f69190144cc491
DESKTOP-DG4M794\Antho <anthonyv@bu.edu>
2020-04-26 15:45:17 -0400

    Added realized and unrealized profits

--- Customer.java
+++ Customer.java
@@ -5,10 +5,22 @@ public class Customer {
     private Checking checking;
     private Saving saving;
     private ArrayList<String> transactions;
+    private double realized; //Realized profit
+    private double unrealized; //Unrealized profit
 
     public Customer(String n){
         name = n;
         transactions = new ArrayList<String>();
+        realized = 0;
+        unrealized = 0;
+    }
+
+    public double getRealizedProfit(){
+        return realized;
+    }
+
+    public double getUnrealizedProfit(){
+        return unrealized;
     }
 
     public void makeSavingAccount(double cash){
@@ -21,21 +33,25 @@ public class Customer {
 
     public void savingWithdraw(double amount){
         this.saving.withdraw(amount);
+        this.realized -= amount;
         this.transactions.add("Savings -" + amount);
     }
 
     public void savingdeposit(double amount){
         this.saving.deposit(amount);
+        this.realized += amount;
         this.transactions.add("Savings +" + amount);
     }
 
     public void checkingWithdraw(double amount){
         this.checking.withdraw(amount);
+        this.realized -= amount;
         this.transactions.add("Checking -" + amount);
     }
 
     public void checkingdeposit(double amount){
         this.checking.deposit(amount);
+        this.realized += amount;
         this.transactions.add("Checking +" + amount);
     }
 
--- RichCustomer.java
+++ RichCustomer.java
@@ -1,21 +1,32 @@
+import java.util.ArrayList;
 
 public class RichCustomer extends Customer{
     private Checking checking;
     private Saving saving;
     private Security security;
+    private ArrayList<String> transactions;
+    private double realized;
+    private double unrealized;
 
-    public RichCustomer(String n, Checking c, Saving s){
+    public RichCustomer(String n, Checking c, Saving s, double r, double u, ArrayList<String> t){
         super(n);
         checking = c;
         saving = s;
+        transactions = t;
+        realized = r;
+        unrealized = u;
     }
 
     public void securityWithdraw(int amount){
         this.checking.withdraw(amount);
+        this.realized -= amount;
+        this.transactions.add("Savings -" + amount);
     }
 
     public void securitydeposit(int amount){
         this.checking.deposit(amount);
+        this.realized += amount;
+        this.transactions.add("Savings -" + amount);
     }
 
     public void buyStock(){}
