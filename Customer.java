4a04b1f0f32eea17de80c5d153e1805a3d6ab081
DESKTOP-DG4M794\Antho <anthonyv@bu.edu>
2020-04-26 11:12:49 -0400

    Added Customer class

---
+++ Customer.java
@@ -0,0 +1,22 @@
+
+public class Customer {
+    private double money;
+    private String name;
+    private Checking checking;
+    private Saving saving;
+    private Securities securities;
+
+    public Customer(int m, String n){
+        money = m;
+        name = n;
+    }
+
+    public void makeSavingAccount(double cash){
+        this.saving = new Saving(cash);
+    }
+
+    public void makeCheckingAccount(double cash){
+        this.checking = new Checking(cash);
+    }
+
+}
