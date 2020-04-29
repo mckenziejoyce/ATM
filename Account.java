eccf9d5278a07dbfe1b35ffc6a375e284ca22495
DESKTOP-DG4M794\Antho <anthonyv@bu.edu>
2020-04-29 13:10:19 -0400

    Added currency exchange function

--- Account.java
+++ Account.java
@@ -1,6 +1,6 @@
 public class Account{
     private double money;
-    private String curr; //Currency
+    private String curr; //Currency for USD, EUR, and JPY
     
     public Account(double m, String c){
         money = m;
@@ -8,7 +8,11 @@ public class Account{
     }
 
     public double getBalance(){
-        return money;
+        return this.money;
+    }
+
+    public String getCurrency(){
+        return this.curr;
     }
 
     public void withdraw(double amount){
@@ -19,7 +23,29 @@ public class Account{
         money += amount;
     }
 
-    //Checks if the transaction is done between to of the
-    //same currencies
-    public static boolean sameCurrency(){return true;}
+    //Converts the the first currency value to the second
+    public static double exchangeRate(String cur1, String cur2, double amount){
+        if(cur1.equals(cur2)){
+            return amount;
+        }
+        else if(cur1.equals("USD") && cur2.equals("EUR")){
+            amount = amount * 0.92;
+        }
+        else if(cur1.equals("EUR") && cur2.equals("USD")){
+            amount = amount * 1.09;
+        }
+        else if(cur1.equals("USD") && cur2.equals("JPY")){
+            amount = amount * 106.69;
+        }
+        else if(cur1.equals("JPY") && cur2.equals("USD")){
+            amount = amount * 0.0094;
+        }
+        else if(cur1.equals("EUR") && cur2.equals("JPY")){
+            amount = amount * 115.74;
+        }
+        else if(cur1.equals("JPY") && cur2.equals("EUR")){
+            amount = amount * 0.0086;
+        }
+        return amount;
+    }
 }
